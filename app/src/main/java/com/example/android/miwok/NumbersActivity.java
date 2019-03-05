57ffdf725d9f8cf8425e089b4bdbd7c564ae6a55
temetm <teemu.makela@fresh-it.fi>
2019-03-05 22:45:17 +0200

    Udacity jatkuu

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,7 +18,10 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -42,16 +45,18 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("Eight");
         words.add("Nine");
         words.add("Ten");
+        words.add("Ten");
+        words.add("Ten");
+        words.add("Ten");
+        words.add("Ten");
+        words.add("Ten");
+        words.add("Ten");
 
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
 
-        int index = 0;
-        while (index < words.size()){
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(index));
-            rootView.addView(wordView);
-            index ++;
-        }
+
 
         //Log.v("Teme words array", "Position 4 = " + words.get(4));
     }
